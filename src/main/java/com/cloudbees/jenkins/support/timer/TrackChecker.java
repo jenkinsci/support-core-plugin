4e36b9215058cc9a05bec57e921bc41b63bbed79
christ66 <schristou88@gmail.com>
2014-04-02 18:15:04 -0700

    Add deadlock detection url calls that are made. Also add time of execution for each web request.

--- src/main/java/com/cloudbees/jenkins/support/timer/TrackChecker.java
+++ src/main/java/com/cloudbees/jenkins/support/timer/TrackChecker.java
@@ -4,6 +4,9 @@ import com.google.inject.Inject;
 import hudson.Extension;
 import hudson.model.PeriodicWork;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadInfo;
+import java.lang.management.ThreadMXBean;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -16,14 +19,38 @@ public class TrackChecker extends PeriodicWork {
 
     @Override
     public long getRecurrencePeriod() {
-        return TimeUnit.MINUTES.toMillis(1);
+        return TimeUnit.SECONDS.toMillis(1);
     }
 
     @Override
     protected void doRun() throws Exception {
         for (Tag t : filter.tracker.values()) {
-            // TODO
+            long stopTime = System.currentTimeMillis();
+            long totalTime = stopTime - t.startTime;
+            System.out.println(t.url + " took: " + totalTime + " milliseconds.");
+
+            if (totalTime > 60000) { // Request to website took over 1 minute
+                ThreadMXBean mbean = ManagementFactory.getThreadMXBean();;
+                long[] deadLocks;
+                try {
+                    deadLocks = mbean.findDeadlockedThreads();
+                } catch (UnsupportedOperationException x) {
+                    deadLocks = null;
+                }
+                if (deadLocks != null && deadLocks.length != 0) {
+                    System.err.println(" Deadlock Found ");
+                    ThreadInfo[] deadLockThreads = mbean.getThreadInfo(deadLocks);
+                    for (ThreadInfo threadInfo : deadLockThreads) {
+                        StackTraceElement[] elements = threadInfo.getStackTrace();
+                        for (StackTraceElement element : elements) {
+                            System.err.println(element.toString());
+                        }
+                    }
+                }
+            }
         }
     }
+
+
 }
 
