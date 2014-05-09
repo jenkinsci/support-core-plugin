3a1a2c3c5671996ccc94681e7bc4cfc89391982f
christ66 <schristou88@gmail.com>
2014-05-09 16:15:33 -0700

    Truncate file if over 2MB in size.

--- src/main/java/com/cloudbees/jenkins/support/timer/FileListCapComponent.java
+++ src/main/java/com/cloudbees/jenkins/support/timer/FileListCapComponent.java
@@ -7,9 +7,10 @@ import edu.umd.cs.findbugs.annotations.NonNull;
 import hudson.Util;
 import hudson.security.Permission;
 import jenkins.model.Jenkins;
+import org.apache.commons.io.FileUtils;
 
-import java.io.File;
-import java.io.FilenameFilter;
+import java.io.*;
+import java.nio.channels.FileChannel;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Set;
@@ -43,9 +44,38 @@ public abstract class FileListCapComponent extends Component {
             for (File f : Util.fixNull(Arrays.asList(files))) {
                 if (f.length() < MAX_FILE_SIZE)
                     container.add(new FileContent(fileListCap.getFolder().getName() + "/" + f.getName(), f));
-                else
+                else {
                     LOGGER.log(Level.INFO, "File : " + f.getAbsolutePath()
-                            + " is too large. Max file size is " + MAX_FILE_SIZE + " bytes.");
+                            + " is too large. Max file size is " + MAX_FILE_SIZE + " bytes. Truncating the thread dump.");
+
+                    File truncatedFile = new File(f.getParent(), "truncated-" + f.getName());
+                    FileChannel fileChannel = null;
+                    try {
+                        FileUtils.copyFile(f, truncatedFile);
+                        // http://stackoverflow.com/questions/14280385/file-truncate-operation-in-java/14280471#14280471
+                        fileChannel = new FileOutputStream(truncatedFile, true).getChannel();
+                        fileChannel.truncate(MAX_FILE_SIZE * 1l); // convert MAX_FILE_SIZE to a long and truncate
+                    } catch (FileNotFoundException e) {
+                        LOGGER.log(Level.INFO, "Unable to find file.", e);
+                    } catch (IOException e) {
+                        LOGGER.log(Level.INFO, "Failed to write to file", e);
+                    } finally {
+                        if (fileChannel != null) {
+                            try {
+                                fileChannel.close();
+                            } catch (IOException e) {
+                                LOGGER.log(Level.FINE, "Failed to close file.", e);
+                            }
+                        }
+                    }
+
+                    if (truncatedFile.exists()) {
+                        container.add(new FileContent(fileListCap.getFolder().getName() + "/" + truncatedFile.getName(),
+                                truncatedFile));
+                    } else {
+                        LOGGER.fine("Issue with truncating file: " + f.getAbsolutePath());
+                    }
+                }
             }
         }
     }
