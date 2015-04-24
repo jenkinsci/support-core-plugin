90efad91e06c234cc4e943d9fbdabec8bf3598c2
Stephen Connolly <stephen.alan.connolly@gmail.com>
2015-04-24 17:11:40 +0100

    License header

--- src/main/java/com/cloudbees/jenkins/support/impl/LoadStats.java
+++ src/main/java/com/cloudbees/jenkins/support/impl/LoadStats.java
@@ -1,3 +1,27 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2015, CloudBees, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 package com.cloudbees.jenkins.support.impl;
 
 import com.cloudbees.jenkins.support.api.Component;
@@ -14,6 +38,7 @@ import hudson.security.Permission;
 import jenkins.model.Jenkins;
 
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.OutputStream;
@@ -64,7 +89,7 @@ public class LoadStats extends Component {
     }
 
     private void add(@NonNull Container container, String name, LoadStatistics stats) {
-        boolean headless = Boolean.getBoolean("java.awt.headless");
+        boolean headless = GraphicsEnvironment.isHeadless();
         for (MultiStageTimeSeries.TimeScale scale: MultiStageTimeSeries.TimeScale.values()) {
             if (!headless) {
                 BufferedImage image = stats.createTrendChart(scale).createChart().createBufferedImage(500, 400);
