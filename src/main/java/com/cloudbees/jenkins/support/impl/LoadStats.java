4cb11299431b1d166832eb0c64b30aae83ad3ed5
Stephen Connolly <stephen.alan.connolly@gmail.com>
2015-04-27 11:30:23 +0100

    Add a gnuplot script if running headless

--- src/main/java/com/cloudbees/jenkins/support/impl/LoadStats.java
+++ src/main/java/com/cloudbees/jenkins/support/impl/LoadStats.java
@@ -59,28 +59,40 @@ import java.util.TimeZone;
 import java.util.TreeMap;
 
 /**
- * @since 
+ * This component captures the Jenkins {@link LoadStatistics} for overall load, jobs not tied to a label and each
+ * label's own load.
+ *
+ * @since 2.21
  */
 @Extension
 public class LoadStats extends Component {
+    /**
+     * {@inheritDoc}
+     */
     @NonNull
     @Override
     public Set<Permission> getRequiredPermissions() {
         return Collections.emptySet();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @NonNull
     @Override
     public String getDisplayName() {
         return "Load Statistics";
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void addContents(@NonNull Container container) {
         Jenkins jenkins = Jenkins.getInstance();
         add(container, "no-label", jenkins.unlabeledLoad);
         add(container, "overall", jenkins.overallLoad);
-        for (Label l: jenkins.getLabels()) {
+        for (Label l : jenkins.getLabels()) {
             try {
                 add(container, String.format("label/%s", URLEncoder.encode(l.getName(), "UTF-8")), l.loadStatistics);
             } catch (UnsupportedEncodingException e) {
@@ -90,8 +102,11 @@ public class LoadStats extends Component {
     }
 
     private void add(@NonNull Container container, String name, LoadStatistics stats) {
+        // A headless environment may be missing the fonts required for these graphs, so even though
+        // we should be able to generate graphs from a headless environment we will skip the graphs
+        // if headless
         boolean headless = GraphicsEnvironment.isHeadless();
-        for (MultiStageTimeSeries.TimeScale scale: MultiStageTimeSeries.TimeScale.values()) {
+        for (MultiStageTimeSeries.TimeScale scale : MultiStageTimeSeries.TimeScale.values()) {
             String scaleName = scale.name().toLowerCase(Locale.ENGLISH);
             if (!headless) {
                 BufferedImage image = stats.createTrendChart(scale).createChart().createBufferedImage(500, 400);
@@ -99,15 +114,23 @@ public class LoadStats extends Component {
             }
             container.add(new CsvContent(String.format("load-stats/%s/%s.csv", name, scaleName), stats, scale));
         }
+        // on the other hand, if headless we should give an easy way to generate the graphs
+        if (headless) {
+            container.add(new GnuPlotScript(String.format("load-stats/%s/gnuplot", name)));
+        }
     }
 
     private static class ImageContent extends Content {
         private final BufferedImage image;
+
         public ImageContent(String name, BufferedImage image) {
             super(name);
             this.image = image;
         }
 
+        /**
+         * {@inheritDoc}
+         */
         @Override
         public void writeTo(OutputStream os) throws IOException {
             ImageIO.write(image, "png", os);
@@ -116,10 +139,17 @@ public class LoadStats extends Component {
 
     private static final List<Field> FIELDS = findFields();
 
+    /**
+     * The fields that a {@link LoadStatistics} has change as you move from pre-1.607 to post 1.607, so better to
+     * just look and see what there is rather than hard-code.
+     *
+     * @return the fields that correspond to {@link MultiStageTimeSeries}
+     */
     private static List<Field> findFields() {
         List<Field> result = new ArrayList<Field>();
-        for (Field f: LoadStatistics.class.getFields()) {
-            if (Modifier.isPublic(f.getModifiers()) && MultiStageTimeSeries.class.isAssignableFrom(f.getType()) && f.getAnnotation(Deprecated.class) == null) {
+        for (Field f : LoadStatistics.class.getFields()) {
+            if (Modifier.isPublic(f.getModifiers()) && MultiStageTimeSeries.class.isAssignableFrom(f.getType())
+                    && f.getAnnotation(Deprecated.class) == null) {
                 result.add(f);
             }
         }
@@ -128,17 +158,17 @@ public class LoadStats extends Component {
 
     private class CsvContent extends PrintedContent {
 
-        private final Map<String,float[]> data;
+        private final Map<String, float[]> data;
         private final long time;
         private final long clock;
-        
+
         public CsvContent(String name, LoadStatistics stats,
                           MultiStageTimeSeries.TimeScale scale) {
             super(name);
             time = System.currentTimeMillis();
             clock = scale.tick;
             data = new TreeMap<String, float[]>();
-            for (Field f: FIELDS) {
+            for (Field f : FIELDS) {
                 try {
                     MultiStageTimeSeries ts = (MultiStageTimeSeries) f.get(stats);
                     if (ts != null) {
@@ -153,28 +183,68 @@ public class LoadStats extends Component {
             }
         }
 
+        /**
+         * {@inheritDoc}
+         */
         @Override
         protected void printTo(PrintWriter out) throws IOException {
             SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
             dateFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
-            
+
             out.print("time");
             int maxLen = 0;
-            for (Map.Entry<String,float[]> entry: data.entrySet()) {
+            for (Map.Entry<String, float[]> entry : data.entrySet()) {
                 out.print(',');
                 out.print(entry.getKey());
                 maxLen = Math.max(maxLen, entry.getValue().length);
             }
             out.println();
-            for (int row = maxLen - 1; row >= 0; row--) {
-                out.print(dateFormat.format(new Date(time - clock*(maxLen - row))));
-                for (float[] h: data.values()) {
+            for (int row = maxLen - 1, offset = 0; row >= 0; row--, offset++) {
+                out.print(dateFormat.format(new Date(time - clock * (maxLen - offset))));
+                for (float[] h : data.values()) {
                     out.print(',');
                     if (h.length > row) {
                         out.print(h[row]);
                     }
                 }
                 out.println();
+            }
+        }
+    }
+
+    private class GnuPlotScript extends PrintedContent {
+
+        public GnuPlotScript(String name) {
+            super(name);
+        }
+
+        @Override
+        protected void printTo(PrintWriter out) throws IOException {
+            // the following is a reasonable approximation of the JFreeChart graphs
+            out.println("#!/usr/bin/env gnuplot");
+            out.println("set style line 101 lc rgb '#000000' lt 1 lw 1;");
+            out.println("set border 3 front ls 101;");
+            out.println("set tics nomirror out scale;");
+            out.println("set format '%g';");
+            out.println("set key box linestyle 101;");
+            out.println("set key outside;");
+            out.println("set key center bottom;");
+            out.println("set key horizontal maxcols 3 spacing 1;");
+            out.println("set timefmt \"%Y/%m/%d %H:%M:%S\";");
+            out.println("set xdata time; ");
+            out.println("set xtics rotate; ");
+            out.println("set datafile sep \",\";");
+            out.println("set term png size 500,400;");
+            for (MultiStageTimeSeries.TimeScale scale : MultiStageTimeSeries.TimeScale.values()) {
+                String scaleName = scale.name().toLowerCase(Locale.ENGLISH);
+                out.printf("set output \"%s.png\";%n", scaleName);
+                int col = 2;
+                for (Field f : FIELDS) {
+                    out.printf("%s \"%s.csv\" using 1:%d with lines title \"%s\"", col == 2 ? "plot" : ",", scaleName,
+                            col, f.getName());
+                    col++;
+                }
+                out.println(";");
             }
         }
     }
