3a2e11dbf8711381a950c21b71c3d208acc3ca79
christ66 <schristou88@gmail.com>
2016-09-07 13:44:05 -0400

    Add proxy information to update center section. Make sure that content from update center is flushed.

--- src/main/java/com/cloudbees/jenkins/support/impl/UpdateCenter.java
+++ src/main/java/com/cloudbees/jenkins/support/impl/UpdateCenter.java
@@ -4,13 +4,17 @@ import com.cloudbees.jenkins.support.api.Component;
 import com.cloudbees.jenkins.support.api.Container;
 import com.cloudbees.jenkins.support.api.Content;
 import com.cloudbees.jenkins.support.api.StringContent;
+import com.ning.http.client.ProxyServer;
 import edu.umd.cs.findbugs.annotations.NonNull;
 import hudson.Extension;
 import hudson.model.UpdateSite;
+import hudson.security.ACL;
 import hudson.security.Permission;
 import jenkins.model.Jenkins;
+import jenkins.plugins.asynchttpclient.AHCUtils;
 
 import java.io.*;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Set;
 
@@ -41,22 +45,35 @@ public class UpdateCenter extends Component {
                     @Override
                     public void writeTo(OutputStream os) throws IOException {
                         PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(os, "utf-8")));
+                        try {
+                            Jenkins instance = Jenkins.getInstance();
+                            if (instance == null) {
+                                out.println("Jenkins has not started yet. No update center information is available.");
+                            } else {
+                                hudson.model.UpdateCenter updateCenter = instance.getUpdateCenter();
+                                out.println("=== Sites ===");
+                                for (UpdateSite c : updateCenter.getSiteList()) {
+                                    out.println(" - Url: " + c.getUrl());
+                                    out.println(" - Connection Url: " + c.getConnectionCheckUrl());
+                                    out.println(" - Implementation Type: " + c.getClass().getName());
+                                }
 
-                        Jenkins instance = Jenkins.getInstance();
-                        if (instance == null) {
-                            out.println("Jenkins has not started yet. No update center information is available.");
-                        } else {
-                            hudson.model.UpdateCenter updateCenter = instance.getUpdateCenter();
-                            out.println("=== Sites ===");
-                            for (UpdateSite c : updateCenter.getSiteList()) {
-                                out.println(" - Url: " + c.getUrl());
-                                out.println(" - Connection Url: " + c.getConnectionCheckUrl());
-                                out.println(" - Implementation Type: " + c.getClass().getName());
+                                out.println("======");
+
+                                out.println("Last updated: " + updateCenter.getLastUpdatedString());
                             }
 
-                            out.println("======");
+                            out.println("=== Proxy ===");
+                            ProxyServer proxyServer = AHCUtils.getProxyServer();
+                            out.println(" - Host: " + proxyServer.getHost());
+                            out.println(" - Port: " + proxyServer.getPort());
 
-                            out.println("Last updated: " + updateCenter.getLastUpdatedString());
+                            out.println(" - No Proxy Hosts: ");
+                            for (String noHost : proxyServer.getNonProxyHosts()) {
+                                out.println(" * " + noHost);
+                            }
+                        } finally {
+                            out.flush();
                         }
                     }
                 }
