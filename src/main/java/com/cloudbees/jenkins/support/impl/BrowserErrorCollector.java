c2ca51560e79b2c5661028ae8ecd174a6dff7def
Michael Neale <michael.neale@gmail.com>
2016-10-31 19:03:40 +1100

    keep it simple.

--- src/main/java/com/cloudbees/jenkins/support/impl/BrowserErrorCollector.java
+++ src/main/java/com/cloudbees/jenkins/support/impl/BrowserErrorCollector.java
@@ -1,7 +1,7 @@
 package com.cloudbees.jenkins.support.impl;
 
 import hudson.Extension;
-import hudson.model.UnprotectedRootAction;
+import hudson.model.RootAction;
 import org.apache.commons.io.IOUtils;
 import org.kohsuke.stapler.Stapler;
 import org.kohsuke.stapler.StaplerProxy;
@@ -11,10 +11,27 @@ import java.io.IOException;
 import java.util.logging.Logger;
 
 /**
- * Created by michaelneale on 24/10/16.
+ * A simple endpoint to collect unhandled browser JS errors.
+ *
+ * To make use of this, install a window.onerror handler,
+ * using a script tag similar to the following in client side javascript code:
+ *  <code>
+ *
+ *            window.onerror = function (msg, url, _lineNo, _columnNo, error) {
+ *                var http = new XMLHttpRequest();
+ *                http.open('POST', "/errorCollector", true);
+ *                http.send(msg + '\n' + url + '\n' + error.stack);
+ *                return false;
+ *            }
+ *
+ *
+ *  </code>
+ *
+ *         This will attempt to log the error with a maybe helpful JS stacktrace.
+ *
  */
 @Extension
-public class BrowserErrorCollector implements UnprotectedRootAction, StaplerProxy {
+public class BrowserErrorCollector implements RootAction, StaplerProxy {
 
     private static final Logger LOGGER = Logger.getLogger(BrowserErrorCollector.class.getName());
 
@@ -37,22 +54,18 @@ public class BrowserErrorCollector implements UnprotectedRootAction, StaplerProx
     public Object getTarget() {
         StaplerRequest request = Stapler.getCurrentRequest();
         try {
-            String msg = IOUtils.toString(request.getInputStream());
-            System.err.println(msg);
-            LOGGER.info(msg);
-
+            javaScriptError(request);
         } catch (IOException e) {
-            LOGGER.severe("Unable to read stream");
+            LOGGER.severe("Unable to log javascript error report. " + e.toString());
         }
-
-        try {
-            Stapler.getCurrentResponse().sendRedirect("/");
-        } catch (IOException e) {
-
-        }
-
-
         return null;
+    }
+
+    private void javaScriptError(StaplerRequest request) throws IOException {
+        LOGGER.warning("Javascript error. User-agent: " +
+                        request.getHeader("User-Agent") +
+                        " -- Error:" +
+                        IOUtils.toString(request.getInputStream()));
     }
 
 
