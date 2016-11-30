19d721e7f9a50d5dc394d11a4471ba385b2172d9
Baptiste Mathus <bmathus@cloudbees.com>
2016-11-30 17:09:35 +0100

    Using FindBugs NonNull instead of Nonnull

--- src/main/java/com/cloudbees/jenkins/support/BundleNameInstanceTypeProvider.java
+++ src/main/java/com/cloudbees/jenkins/support/BundleNameInstanceTypeProvider.java
@@ -26,11 +26,11 @@ package com.cloudbees.jenkins.support;
 
 import com.cloudbees.jenkins.support.api.SupportProvider;
 import com.google.common.annotations.VisibleForTesting;
+import edu.umd.cs.findbugs.annotations.NonNull;
 import hudson.Extension;
 import hudson.ExtensionList;
 import hudson.ExtensionPoint;
 
-import javax.annotation.Nonnull;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -57,7 +57,7 @@ public abstract class BundleNameInstanceTypeProvider implements ExtensionPoint {
 
     private static final Logger LOGGER = Logger.getLogger(BundleNameInstanceTypeProvider.class.getName());
 
-    @Nonnull
+    @NonNull
     static BundleNameInstanceTypeProvider getInstance() {
         final ExtensionList<BundleNameInstanceTypeProvider> all = ExtensionList.lookup(BundleNameInstanceTypeProvider.class);
         final int extensionCount = all.size();
@@ -91,7 +91,7 @@ public abstract class BundleNameInstanceTypeProvider implements ExtensionPoint {
      *
      * @return the instance type specification to be used for generated support bundles.
      */
-    @Nonnull
+    @NonNull
     public abstract String getInstanceType();
 
     // We want this to be always picked up last in case others are found Double.MIN_VALUE does NOT work
