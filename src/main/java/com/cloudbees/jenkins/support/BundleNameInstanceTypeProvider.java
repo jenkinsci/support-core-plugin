c41b3be65aaa5890b69e6222ab399b8f3e9b1283
Baptiste Mathus <bmathus@cloudbees.com>
2016-11-30 15:48:32 +0100

    Simplify approach using Extension for default behaviour too
    And that extension get a very low ordinal to be always chosen last.
    
    WARNING: Double.MIN_VALUE does NOT work...

--- src/main/java/com/cloudbees/jenkins/support/BundleNameInstanceTypeProvider.java
+++ src/main/java/com/cloudbees/jenkins/support/BundleNameInstanceTypeProvider.java
@@ -1,7 +1,32 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2016, CloudBees, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 package com.cloudbees.jenkins.support;
 
 import com.cloudbees.jenkins.support.api.SupportProvider;
 import com.google.common.annotations.VisibleForTesting;
+import hudson.Extension;
 import hudson.ExtensionList;
 import hudson.ExtensionPoint;
 
@@ -32,40 +57,33 @@ public abstract class BundleNameInstanceTypeProvider implements ExtensionPoint {
 
     private static final Logger LOGGER = Logger.getLogger(BundleNameInstanceTypeProvider.class.getName());
 
-    private static final BundleNameInstanceTypeProvider DEFAULT_STRATEGY = new BundleNameInstanceTypeProvider() {
-
-        @Override
-        public String getInstanceType() {
-            return System.getProperty(SUPPORT_BUNDLE_NAMING_INSTANCE_SPEC_PROPERTY, "");
-        }
-    };
-
-    /* package */
     @Nonnull
     static BundleNameInstanceTypeProvider getInstance() {
         final ExtensionList<BundleNameInstanceTypeProvider> all = ExtensionList.lookup(BundleNameInstanceTypeProvider.class);
         final int extensionCount = all.size();
-        if (extensionCount < 1) {
-            LOGGER.fine("No alternative strategy provided for support bundle prefixing.");
-            return DEFAULT_STRATEGY;
-        }
 
-        if (all.size() > 1) {
+        if (extensionCount > 2) {
             if (LOGGER.isLoggable(Level.WARNING)) {
-                LOGGER.warning("{0} implementations found for support bundle prefix naming strategy. " +
-                        "Can be only 0 or 1. Choosing the first found.");
-                for (BundleNameInstanceTypeProvider bundlePrefixProvider : all) {
-                    LOGGER.log(Level.WARNING, "class '{0}' found", bundlePrefixProvider.getClass().getName());
+                LOGGER.log(Level.WARNING, "{0} implementations found for support bundle prefix naming strategy. " +
+                        "Can be only 1 (default one) or 2 (default one, plus alternative). " +
+                        "Choosing the first found among the following:", extensionCount);
+                for (BundleNameInstanceTypeProvider nameProvider : all) {
+                    LOGGER.log(Level.WARNING, "Class {0} found", nameProvider.getClass().getName());
                 }
             }
         }
-        return all.get(0);
+
+        final BundleNameInstanceTypeProvider chosen = all.get(0);
+        if (extensionCount > 1) {
+            LOGGER.log(Level.INFO, "Using {0} as BundleNameInstanceTypeProvider implementation", chosen.getClass().getName());
+        }
+        return chosen;
     }
 
     /**
      * Returns the <strong>non-null</strong> instance type to be used for generated support bundle names.
      * Aims to provide informational data about the generated bundles.
-     *
+     * <p>
      * <p>
      * <p><b>Will be used for file name generation, so avoid funky characters.
      * Please stay in <code>[a-zA-Z-_]</code></b>. Also consider the file name length, you probably want to be defensive
@@ -75,4 +93,14 @@ public abstract class BundleNameInstanceTypeProvider implements ExtensionPoint {
      */
     @Nonnull
     public abstract String getInstanceType();
+
+    // We want this to be always picked up last in case others are found Double.MIN_VALUE does NOT work
+    @Extension(ordinal = -1000000)
+    public static final class DEFAULT_STRATEGY extends BundleNameInstanceTypeProvider {
+
+        @Override
+        public String getInstanceType() {
+            return System.getProperty(BundleNameInstanceTypeProvider.SUPPORT_BUNDLE_NAMING_INSTANCE_SPEC_PROPERTY, "");
+        }
+    }
 }
