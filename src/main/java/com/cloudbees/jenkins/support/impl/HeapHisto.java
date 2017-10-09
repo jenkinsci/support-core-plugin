438a2287f42bfe68ea556c5c05b7a223f101e06d
Emilio Escobar <escoem@users.noreply.github.com>
2017-10-09 13:06:43 +0200

    [JENKINS-2279] comments addressed

--- src/main/java/com/cloudbees/jenkins/support/impl/HeapHisto.java
+++ src/main/java/com/cloudbees/jenkins/support/impl/HeapHisto.java
@@ -11,9 +11,12 @@ import hudson.remoting.Callable;
 import hudson.security.Permission;
 import jenkins.model.Jenkins;
 import org.jenkinsci.remoting.RoleChecker;
+import org.kohsuke.accmod.Restricted;
+import org.kohsuke.accmod.restrictions.NoExternalUse;
 
 import javax.management.InstanceNotFoundException;
 import javax.management.MBeanException;
+import javax.management.MBeanServer;
 import javax.management.MalformedObjectNameException;
 import javax.management.ObjectName;
 import javax.management.ReflectionException;
@@ -26,7 +29,11 @@ import java.util.WeakHashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+/**
+ * Heap histogram from master node.
+ */
 @Extension
+@Restricted(NoExternalUse.class)
 public class HeapHisto extends Component {
     private static final int OFFSET = 3;
     private static final int MAX = 200 + OFFSET;
@@ -43,7 +50,7 @@ public class HeapHisto extends Component {
     @NonNull
     @Override
     public String getDisplayName() {
-        return "Master Heap Histogram -histo:live";
+        return "Master Heap Histogram";
     }
 
     @Override
@@ -74,7 +81,7 @@ public class HeapHisto extends Component {
 
             final StringBuilder bos = new StringBuilder();
 
-            bos.append("Master Heap Histogram -histo:live");
+            bos.append("Master Heap Histogram");
             for (int i=0; i<limit; i++) {
                 bos.append(lines[i]).append('\n');
             }
@@ -92,11 +99,15 @@ public class HeapHisto extends Component {
             String result;
             try {
                 ObjectName objName = new ObjectName("com.sun.management:type=DiagnosticCommand");
-                result = (String) ManagementFactory.getPlatformMBeanServer().invoke(objName, "gcClassHistogram", new Object[] {null}, new String[]{String[].class.getName()});
+                MBeanServer platform = ManagementFactory.getPlatformMBeanServer();
+                if (platform == null) {
+                    return "N/A";
+                }
+                result = (String) platform.invoke(objName, "gcClassHistogram", new Object[] {null}, new String[]{String[].class.getName()});
             }
             catch (InstanceNotFoundException | ReflectionException | MBeanException | MalformedObjectNameException e) {
                 logger.log(Level.WARNING,"Could not record heap live histogram.", e);
-                result = "NA";
+                result = "N/A";
             }
             return result;
         }
