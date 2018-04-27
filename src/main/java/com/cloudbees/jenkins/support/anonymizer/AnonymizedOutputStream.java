1004b77c3d4ba0423b8fd3dbf344b447f9b90e66
Matt Sicker <boards@gmail.com>
2018-04-27 15:26:35 -0500

    Add ContentFilter API
    This adds the ContentFilter interface along with an OutputStream wrapper that filters written contents using the provided filter.
    
    Related to JENKINS-21670.

---
+++ src/main/java/com/cloudbees/jenkins/support/anonymizer/AnonymizedOutputStream.java
@@ -0,0 +1,192 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2018, CloudBees, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.cloudbees.jenkins.support.anonymizer;
+
+import javax.annotation.concurrent.GuardedBy;
+import java.io.FilterOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+import java.nio.charset.CoderResult;
+import java.nio.charset.CodingErrorAction;
+import java.nio.charset.StandardCharsets;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Wraps an OutputStream by filtering written lines using a provided ContentFilter.
+ * Data written to an instance of this will be decoded on the fly using the provided charset, and each line
+ * written is filtered.
+ *
+ * @author Matt Sicker
+ * @see ContentFilter
+ * @since 2.48
+ */
+public class AnonymizedOutputStream extends FilterOutputStream {
+
+    private static final Pattern EOL = Pattern.compile("\r?\n");
+    private static final int DEFAULT_DECODER_CAPACITY = 1024;
+
+    @GuardedBy("this")
+    private final ByteBuffer in = ByteBuffer.allocate(256);
+    @GuardedBy("this")
+    private CharBuffer buf = CharBuffer.allocate(DEFAULT_DECODER_CAPACITY);
+    private final Charset charset;
+    @GuardedBy("this")
+    private final CharsetDecoder decoder;
+    private final ContentFilter contentFilter;
+
+    /**
+     * Constructs a filtered stream using the provided filter and assuming UTF-8.
+     *
+     * @param out           output stream to write filtered content to
+     * @param contentFilter content filter to apply to lines written through this stream
+     */
+    public AnonymizedOutputStream(OutputStream out, ContentFilter contentFilter) {
+        this(out, StandardCharsets.UTF_8, contentFilter);
+    }
+
+    /**
+     * Constructs a filtered stream using the provided filter and charset.
+     *
+     * @param out           output stream to write filtered content to
+     * @param charset       character set to use for decoding and encoding bytes written to this stream
+     * @param contentFilter content filter to apply to lines written through this stream
+     */
+    public AnonymizedOutputStream(OutputStream out, Charset charset, ContentFilter contentFilter) {
+        super(out);
+        this.charset = charset;
+        this.decoder = charset.newDecoder()
+                .onMalformedInput(CodingErrorAction.REPLACE)
+                .onUnmappableCharacter(CodingErrorAction.REPLACE)
+                .replaceWith("\uFFFD");
+        this.contentFilter = contentFilter;
+    }
+
+    @Override
+    public synchronized void write(int b) throws IOException {
+        write(new byte[]{(byte) b}, 0, 1);
+    }
+
+    @Override
+    public synchronized void write(byte[] b) throws IOException {
+        write(b, 0, b.length);
+    }
+
+    @Override
+    public synchronized void write(byte[] b, int off, int len) throws IOException {
+        while (len > 0) {
+            int toCopy = Math.min(in.remaining(), len);
+            if (toCopy == 0) throw new IllegalStateException();
+            in.put(b, off, toCopy);
+            decodeFilterFlushLines(false);
+            len -= toCopy;
+            off += toCopy;
+        }
+        filterFlushLines();
+    }
+
+    /**
+     * Flushes the current buffered contents and filters them as is.
+     */
+    @Override
+    public synchronized void flush() throws IOException {
+        if (buf.position() > 0) {
+            buf.flip();
+            String contents = buf.toString();
+            String filtered = contentFilter.filter(contents);
+            out.write(filtered.getBytes(charset));
+            buf.clear();
+            if (buf.capacity() > DEFAULT_DECODER_CAPACITY) {
+                buf = CharBuffer.allocate(DEFAULT_DECODER_CAPACITY);
+            }
+        }
+        out.flush();
+    }
+
+    @Override
+    public synchronized void close() throws IOException {
+        decodeFilterFlushLines(true);
+        flush();
+        reset();
+        out.close();
+    }
+
+    private void decodeFilterFlushLines(boolean endOfInput) throws IOException {
+        in.flip();
+        while (true) {
+            CoderResult result = decoder.decode(in, buf, endOfInput);
+            if (result.isUnderflow()) {
+                // keep accepting; can't decode this yet
+                break;
+            } else if (result.isOverflow()) {
+                if (!filterFlushLines()) {
+                    // unable to make space, need to resize
+                    buf.flip();
+                    buf = CharBuffer.allocate(buf.capacity() + DEFAULT_DECODER_CAPACITY).put(buf);
+                }
+            } else {
+                throw new IllegalStateException("CharsetDecoder is mis-configured. Result: " + result);
+            }
+        }
+        in.compact();
+    }
+
+    private boolean filterFlushLines() throws IOException {
+        boolean flushed = false;
+        if (buf.position() > 0) {
+            buf.flip();
+            Matcher matcher = EOL.matcher(buf);
+            int start = 0;
+            while (matcher.find()) {
+                int end = matcher.end();
+                String line = buf.subSequence(start, end).toString();
+                String filtered = contentFilter.filter(line);
+                out.write(filtered.getBytes(charset));
+                start = end;
+                flushed = true;
+            }
+            buf.position(start);
+            if (buf.capacity() - buf.remaining() > DEFAULT_DECODER_CAPACITY) {
+                buf = CharBuffer.allocate(DEFAULT_DECODER_CAPACITY).put(buf);
+            } else {
+                buf.compact();
+            }
+        }
+        return flushed;
+    }
+
+    /**
+     * Resets the state of this stream's decoders and buffers.
+     */
+    public synchronized void reset() {
+        in.clear();
+        buf.clear();
+        decoder.reset();
+    }
+}
---
+++ src/test/java/com/cloudbees/jenkins/support/impl/AnonymizedOutputStreamTest.java
@@ -0,0 +1,80 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2018, CloudBees, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.cloudbees.jenkins.support.impl;
+
+import com.cloudbees.jenkins.support.anonymizer.AnonymizedOutputStream;
+import com.cloudbees.jenkins.support.anonymizer.ContentFilter;
+import org.apache.commons.io.IOUtils;
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.junit.Assert.*;
+
+public class AnonymizedOutputStreamTest {
+
+    private final InputStream testInput = getClass().getResourceAsStream("/test-input.txt");
+    private final ByteArrayOutputStream testOutput = new ByteArrayOutputStream();
+
+    @Test
+    public void shouldModifyStream() throws IOException {
+        assertNotNull(testInput);
+        ContentFilter filter = s -> s.replace("Line", "Network");
+        AnonymizedOutputStream out = new AnonymizedOutputStream(testOutput, filter);
+
+        IOUtils.copy(testInput, out);
+        String contents = new String(testOutput.toByteArray(), UTF_8);
+
+        assertNotEquals(0, contents.length());
+        String[] lines = contents.split("\n");
+        for (int i = 0; i < lines.length; i++) {
+            assertEquals(String.format("Network %d", i), lines[i]);
+        }
+    }
+
+    @Test
+    public void shouldSupportLongLines() throws IOException {
+        char[] input = new char[4096];
+        Arrays.fill(input, '*');
+        InputStream in = new ByteArrayInputStream(new String(input).getBytes(UTF_8));
+        AnonymizedOutputStream out = new AnonymizedOutputStream(testOutput, s -> s.replace('*', 'a'));
+
+        IOUtils.copy(in, out);
+        String contents = new String(testOutput.toByteArray(), UTF_8);
+
+        assertEquals("Buffer overflowed prematurely", 0, contents.length());
+
+        out.flush();
+        contents = new String(testOutput.toByteArray(), UTF_8);
+
+        assertNotEquals("No contents written", 0, contents.length());
+        assertTrue(contents.matches("^a{4096}$"));
+    }
+}
