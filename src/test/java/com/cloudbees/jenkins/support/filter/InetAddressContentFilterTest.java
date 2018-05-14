a6b6c07c0dc664ef61d035d95f99aeb0b05e20df
Matt Sicker <boards@gmail.com>
2018-05-14 15:08:57 -0500

    Add property tests for IP address filtering

--- src/test/java/com/cloudbees/jenkins/support/filter/InetAddressContentFilterTest.java
+++ src/test/java/com/cloudbees/jenkins/support/filter/InetAddressContentFilterTest.java
@@ -24,59 +24,64 @@
 
 package com.cloudbees.jenkins.support.filter;
 
+import hudson.BulkChange;
 import org.junit.ClassRule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameters;
 import org.jvnet.hudson.test.Issue;
 import org.jvnet.hudson.test.JenkinsRule;
+import org.quicktheories.core.Gen;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
+import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.quicktheories.QuickTheory.qt;
+import static org.quicktheories.generators.SourceDSL.integers;
 
-@RunWith(Parameterized.class)
 public class InetAddressContentFilterTest {
 
     @ClassRule
     public static JenkinsRule jenkins = new JenkinsRule();
 
-    private final String input;
-    private final boolean shouldFilter;
-
-    public InetAddressContentFilterTest(String input, boolean shouldFilter) {
-        this.input = input;
-        this.shouldFilter = shouldFilter;
-    }
-
-    @Parameters(name = "{0}")
-    public static Object[][] data() throws UnknownHostException {
-        return new Object[][] {
-                {"127.0.0.1", true},
-                {"0.0.0.0", true},
-                {"256.0.0.1", false},
-                {"255.255.255.255", true},
-                {"http://192.168.10.100/", true},
-                {"192:168::1", true},
-                {"ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff", true},
-                {"inva:lid", false},
-                {"10::1", true},
-                {"http://[10::1]:8080/", true},
-                {InetAddress.getLocalHost().getHostAddress(), true}
-        };
+    @Issue("JENKINS-21670")
+    @Test
+    public void shouldFilterIPv4Addresses() {
+        InetAddressContentFilter filter = InetAddressContentFilter.get();
+        // speed up test execution by ignoring new content mappings
+        ContentMappings mappings = ContentMappings.get();
+        try (BulkChange ignored = new BulkChange(mappings)) {
+            qt().forAll(ipv4()).checkAssert(string ->
+                    assertThat(filter.filter(string))
+                            .contains("ip_")
+                            .doesNotContain(string)
+            );
+        }
     }
 
     @Issue("JENKINS-21670")
     @Test
-    public void testFilter() {
+    public void shouldFilterIPv6Addresses() {
         InetAddressContentFilter filter = InetAddressContentFilter.get();
-        String filtered = filter.filter(input);
-        if (shouldFilter) {
-            assertThat(filtered).contains("ip_").doesNotContain(input);
-        } else {
-            assertThat(filtered).isEqualTo(input);
+        // speed up test execution by ignoring new content mappings
+        ContentMappings mappings = ContentMappings.get();
+        try (BulkChange ignored = new BulkChange(mappings)) {
+            qt().forAll(ipv6()).checkAssert(string ->
+                    assertThat(filter.filter(string))
+                            .contains("ip_")
+                            .doesNotContain(string)
+            );
         }
+    }
+
+    private Gen<String> ipv4() {
+        Gen<String> octet = integers().between(0, 255).map(Number::toString);
+        return octet.zip(octet, octet, octet, (p1, p2, p3, p4) -> p1 + '.' + p2 + '.' + p3 + '.' + p4);
+    }
+
+    private Gen<String> ipv6() {
+        Gen<String> part = integers().between(0, 65535).map(Integer::toHexString);
+        return IntStream.range(0, 8)
+                .mapToObj(i -> part)
+                .reduce((left, right) -> left.zip(right, (s1, s2) -> s1 + ':' + s2))
+                .orElseThrow(IllegalStateException::new);
     }
 }
