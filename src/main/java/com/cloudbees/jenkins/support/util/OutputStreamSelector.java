d8e8170f2f470ad308879d3e0f382236fb82ade2
Matt Sicker <boards@gmail.com>
2018-05-16 16:43:13 -0500

    Add OutputStreamSelector for dynamically choosing a binary or text output stream

---
+++ src/main/java/com/cloudbees/jenkins/support/util/OutputStreamSelector.java
@@ -0,0 +1,133 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2018, CloudBees, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.cloudbees.jenkins.support.util;
+
+import org.kohsuke.accmod.Restricted;
+import org.kohsuke.accmod.restrictions.NoExternalUse;
+
+import javax.annotation.Nonnull;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
+import java.util.function.Supplier;
+
+import static java.util.Objects.requireNonNull;
+
+@Restricted(NoExternalUse.class)
+public class OutputStreamSelector extends OutputStream {
+    private static final int DEFAULT_PROBE_SIZE = 20;
+    private final Supplier<OutputStream> binaryOutputStreamProvider;
+    private final Supplier<OutputStream> textOutputStreamProvider;
+    private ByteBuffer head = ByteBuffer.allocate(DEFAULT_PROBE_SIZE);
+    private OutputStream out;
+    private boolean closed;
+    private boolean flushScheduled;
+
+    public OutputStreamSelector(@Nonnull Supplier<OutputStream> binaryOutputStreamProvider, @Nonnull Supplier<OutputStream> textOutputStreamProvider) {
+        this.binaryOutputStreamProvider = binaryOutputStreamProvider;
+        this.textOutputStreamProvider = textOutputStreamProvider;
+    }
+
+    private void ensureOpen() {
+        if (closed) throw new IllegalStateException("Stream is closed");
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+        ensureOpen();
+        write(new byte[]{(byte) b});
+    }
+
+    @Override
+    public void write(byte[] b, int off, int len) throws IOException {
+        ensureOpen();
+        if (len < 0) throw new IllegalArgumentException("Length cannot be negative. Got: " + len);
+        if (len == 0) return;
+        if (out == null) {
+            probeContents(b, off, len);
+        } else {
+            out.write(b, off, len);
+        }
+    }
+
+    private void probeContents(byte[] b, int off, int len) throws IOException {
+        int toCopy = Math.min(head.remaining(), len);
+        if (toCopy == 0) throw new IllegalStateException("No more room to buffer header, should have chosen stream by now");
+        head.put(b, off, toCopy);
+        if (head.hasRemaining()) return;
+        chooseStream();
+        if (toCopy < len) {
+            write(b, off + toCopy, len - toCopy);
+        }
+    }
+
+    private void chooseStream() throws IOException {
+        if (head.position() > 0) {
+            head.flip().mark();
+            boolean hasControlCharacter = false;
+            while (head.hasRemaining()) {
+                hasControlCharacter |= isNonWhitespaceControlCharacter(head.get());
+            }
+            head.reset();
+            out = requireNonNull((hasControlCharacter ? binaryOutputStreamProvider : textOutputStreamProvider).get(), "No OutputStream returned by supplier");
+            byte[] b = new byte[head.remaining()];
+            head.get(b);
+            write(b);
+        } else {
+            out = binaryOutputStreamProvider.get();
+        }
+        if (flushScheduled) {
+            out.flush();
+        }
+    }
+
+    private static boolean isNonWhitespaceControlCharacter(byte b) {
+        char c = (char) (b & 0xff);
+        return Character.isISOControl(c) && c != '\t' && c != '\n' && c != '\r';
+    }
+
+    @Override
+    public void flush() throws IOException {
+        ensureOpen();
+        if (out == null) {
+            flushScheduled = true;
+        } else {
+            out.flush();
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        ensureOpen();
+        try {
+            if (out == null) {
+                chooseStream();
+            }
+            out.close();
+        } finally {
+            closed = true;
+        }
+    }
+}
---
+++ src/test/java/com/cloudbees/jenkins/support/util/OutputStreamSelectorTest.java
@@ -0,0 +1,135 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2018, CloudBees, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.cloudbees.jenkins.support.util;
+
+import com.github.javafaker.Faker;
+import org.apache.commons.io.output.ByteArrayOutputStream;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+public class OutputStreamSelectorTest {
+
+    private ByteArrayOutputStream binaryOut = spy(new ByteArrayOutputStream());
+    private ByteArrayOutputStream textOut = spy(new ByteArrayOutputStream());
+    private OutputStreamSelector selector = new OutputStreamSelector(() -> binaryOut, () -> textOut);
+
+    @Test
+    public void shouldNotAllowWritesAfterClose() throws IOException {
+        selector.close();
+        assertThatIllegalStateException()
+                .isThrownBy(() -> selector.write(0))
+                .withMessageContaining("closed");
+        assertThatIllegalStateException()
+                .isThrownBy(() -> selector.write(new byte[]{0, 1, 2, 3, 4, 5, 6, 7}))
+                .withMessageContaining("closed");
+        assertThatIllegalStateException()
+                .isThrownBy(() -> selector.write(new byte[]{0, 1, 2, 3, 4, 5, 6, 7}, 4, 4))
+                .withMessageContaining("closed");
+    }
+
+    @Test
+    public void shouldNotAllowInvalidLengths() throws IOException {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> selector.write(new byte[0], 0, -1));
+    }
+
+    @Test
+    public void shouldIgnoreEmptyWritesWhereValid() throws IOException {
+        selector.write(new byte[0]);
+        selector.write(new byte[0], 0, 0);
+        selector.close();
+
+        assertThatIllegalStateException()
+                .isThrownBy(() -> selector.write(new byte[0]));
+        assertThat(binaryOut.toByteArray()).isEmpty();
+        assertThat(textOut.toByteArray()).isEmpty();
+    }
+
+    @Test
+    public void shouldUseBinaryStreamWhenNonPrintableCharactersFoundEarly() throws IOException {
+        byte[] bytes = new byte[]{0x41, 0x42, 0x43, 0x44, 0x4, 0x46, 0x47, 0x48, 0x49, 0x50}; // 0x4 is EOT
+
+        selector.write(bytes);
+        selector.close();
+
+        assertThat(binaryOut.toByteArray()).isNotEmpty().isEqualTo(bytes);
+    }
+
+    @Test
+    public void shouldUseTextStreamWhenAllPrintable() throws IOException {
+        Faker faker = new Faker();
+        String contents = faker.lorem().paragraph();
+
+        selector.write(contents.getBytes(UTF_8));
+        selector.close();
+
+        assertThat(binaryOut.toByteArray()).isEmpty();
+        byte[] bytes = textOut.toByteArray();
+        assertThat(bytes).isNotEmpty();
+        String actual = new String(bytes, UTF_8);
+        assertThat(actual).isEqualTo(contents);
+    }
+
+    @Test
+    public void shouldDeferFlushUntilStreamChosen() throws IOException {
+        selector.write(0x42);
+        selector.flush();
+        verify(binaryOut, never()).flush();
+        verify(textOut, never()).flush();
+        selector.close();
+        verify(textOut).flush();
+        verifyNoMoreInteractions(binaryOut);
+    }
+
+    @Test
+    public void shouldPassAlongFlushAfterStreamChosenPreviously() throws IOException {
+        for (int i = 0; i < 1024; i++) {
+            selector.write(0);
+        }
+        verify(binaryOut, never()).flush();
+        selector.flush();
+        verify(binaryOut).flush();
+        verifyNoMoreInteractions(textOut);
+    }
+
+    @Test
+    public void shouldUseBinaryStreamForPNG() throws IOException {
+        Path pngFile = Paths.get("src/main/webapp/images/16x16/support.png");
+        ByteArrayOutputStream expectedStreamContents = new ByteArrayOutputStream();
+        Files.copy(pngFile, expectedStreamContents);
+        Files.copy(pngFile, selector);
+        byte[] expected = expectedStreamContents.toByteArray();
+        assertThat(textOut.toByteArray()).isEmpty();
+        assertThat(binaryOut.toByteArray()).isNotEmpty().isEqualTo(expected);
+    }
+}
