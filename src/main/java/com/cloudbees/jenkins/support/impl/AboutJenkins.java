a119bbfafbd8c0089fa2702f45df185a5d7e2cd0
fbelzunc@gmail.com <fbelzunc@gmail.com>
2019-04-10 14:42:24 +0200

    [JENKINS-33628] Remove Stats class from AboutJenkins

--- src/main/java/com/cloudbees/jenkins/support/impl/AboutJenkins.java
+++ src/main/java/com/cloudbees/jenkins/support/impl/AboutJenkins.java
@@ -477,62 +477,6 @@ public class AboutJenkins extends Component {
         return format.format(number) + " " + measure + " (" + size + ")";
     }
 
-    private static class Stats {
-        private int s0 = 0;
-        private long s1 = 0;
-        private long s2 = 0;
-
-        public synchronized void add(int x) {
-            s0++;
-            s1 += x;
-            s2 += x * (long) x;
-        }
-
-        public synchronized double x() {
-            return s1 / (double) s0;
-        }
-
-        private static double roundToSigFig(double num, int sigFig) {
-            if (num == 0) {
-                return 0;
-            }
-            final double d = Math.ceil(Math.log10(num < 0 ? -num : num));
-            final int pow = sigFig - (int) d;
-            final double mag = Math.pow(10, pow);
-            final long shifted = Math.round(num * mag);
-            return shifted / mag;
-        }
-
-        public synchronized double s() {
-            if (s0 >= 2) {
-                double v = Math.sqrt((s0 * (double) s2 - s1 * (double) s1) / s0 / (s0 - 1));
-                if (s0 <= 100) {
-                    return roundToSigFig(v, 1); // 0.88*SD to 1.16*SD
-                }
-                if (s0 <= 1000) {
-                    return roundToSigFig(v, 2); // 0.96*SD to 1.05*SD
-                }
-                return v;
-            } else {
-                return Double.NaN;
-            }
-        }
-
-        public synchronized String toString() {
-            if (s0 == 0) {
-                return "N/A";
-            }
-            if (s0 == 1) {
-                return Long.toString(s1) + " [n=" + s0 + "]";
-            }
-            return Double.toString(x()) + " [n=" + s0 + ", s=" + s() + "]";
-        }
-
-        public synchronized int n() {
-            return s0;
-        }
-    }
-
     private static class AboutContent extends PrintedContent {
         private final Iterable<PluginWrapper> plugins;
 
