f447df4215f6e6178421ad7983b1229274faeb24
fbelzunc <fbelzunc@gmail.com>
2020-09-18 16:18:11 +0200

    [JENKINS-63674] Implement the thread dump feature on high cpu

---
+++ src/main/java/com/cloudbees/jenkins/support/threaddump/HighLoadChecker.java
@@ -0,0 +1,103 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2020 CloudBees, Inc
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.cloudbees.jenkins.support.threaddump;
+
+import com.cloudbees.jenkins.support.impl.ThreadDumps;
+import com.cloudbees.jenkins.support.timer.FileListCap;
+import com.codahale.metrics.Gauge;
+import com.codahale.metrics.MetricSet;
+import hudson.Extension;
+import hudson.ExtensionList;
+import hudson.model.PeriodicWork;
+import jenkins.metrics.api.MetricProvider;
+import jenkins.metrics.impl.VMMetricProviderImpl;
+import jenkins.model.Jenkins;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * PeriodicWork to check when there is a high load in the instance.
+ *
+ * Only checking high CPU usage for the moment, but it can be used
+ * to generate thread dumps in high heap memory consumption.
+ */
+@Extension
+public class HighLoadChecker extends PeriodicWork {
+
+    /**
+     * Recurrence period to check high load consumption and take the corresponded actions
+     * like generating a thread dump
+     */
+    public static final int RECURRENCE_PERIOD_SEC =
+            Integer.getInteger(HighLoadChecker.class.getName()+ ".RECURRENCE_PERIOD_SEC", 900);
+
+    /**
+     * This is the CPU usage threshold
+     */
+    public static final Double CPU_USAGE_THRESHOLD =
+            new Double(System.getProperty(HighLoadChecker.class.getName() + ".CPU_USAGE_THRESHOLD", "0.95"));
+
+    /**
+     * Limit the number of thread dumps to retain on high cpu
+     */
+    public static final int HIGH_CPU_THEAD_DUMPS_TO_RETAIN =
+            Integer.getInteger(HighLoadChecker.class.getName()+ ".HIGH_CPU_THEAD_DUMPS_TO_RETAIN", 10);
+
+    /**
+     * Thread dumps generated on high CPU load are stored in $JENKINS_HOME/high-load/cpu
+     **/
+    final FileListCap logs = new FileListCap(new File(Jenkins.getInstanceOrNull().getRootDir(),"high-load/cpu"), HIGH_CPU_THEAD_DUMPS_TO_RETAIN);
+
+    final SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd-HHmmss.SSS");
+    {
+        format.setTimeZone(TimeZone.getTimeZone("UTC"));
+    }
+
+    @Override
+    public long getRecurrencePeriod() {
+        return TimeUnit.SECONDS.toMillis(RECURRENCE_PERIOD_SEC);
+    }
+
+    @Override
+    protected void doRun() throws Exception {
+        VMMetricProviderImpl vmMetricProvider = ExtensionList.lookup(MetricProvider.class).get(VMMetricProviderImpl.class);
+        MetricSet vmProviderMetricSet = vmMetricProvider.getMetricSet();
+        Gauge cpu = (Gauge) vmProviderMetricSet.getMetrics().get("vm.cpu.load");
+        Double cpuLoad = new Double(cpu.getValue().toString());
+
+        if (Double.compare(CPU_USAGE_THRESHOLD, cpuLoad) < 0) {
+            File threadDumpFile = logs.file(format.format(new Date()) + ".txt");
+            logs.add(threadDumpFile);
+            FileOutputStream fileOutputStream = new FileOutputStream(threadDumpFile);
+            ThreadDumps.threadDump(fileOutputStream);
+            fileOutputStream.close();
+        }
+    }
+}
--- src/main/java/com/cloudbees/jenkins/support/threaddump/ThreadDumpHighCPU.java
+++ src/main/java/com/cloudbees/jenkins/support/threaddump/ThreadDumpHighCPU.java
@@ -1,26 +1,50 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2020 CloudBees, Inc
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 package com.cloudbees.jenkins.support.threaddump;
 
 import com.cloudbees.jenkins.support.api.Container;
 import com.cloudbees.jenkins.support.timer.UnfilteredFileListCapComponent;
+import com.google.inject.Inject;
 import edu.umd.cs.findbugs.annotations.NonNull;
 import hudson.Extension;
 
 /**
- * Component which automatically generates thread dumps on high CPU
- *
+ * ThreadDumpHighCPU component
  */
-
 @Extension
 public class ThreadDumpHighCPU extends UnfilteredFileListCapComponent {
 
-    @NonNull
-    @Override
+    @Inject
+    HighLoadChecker checker;
+
     public String getDisplayName() {
         return Messages.ThreadDumpHighCPU_DisplayName();
     }
 
     @Override
     public void addContents(@NonNull Container container) {
-
+        super.addContents(container, checker.logs);
     }
 }
