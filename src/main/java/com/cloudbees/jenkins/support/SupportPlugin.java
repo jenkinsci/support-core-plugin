d02b2a9a0dfc41fe796c31c10c78427994cdadb7
fbelzunc <fbelzunc@gmail.com>
2020-12-11 15:26:18 +0100

    [JENKINS-64401] Remove import for FilterOutputStream

--- src/main/java/com/cloudbees/jenkins/support/SupportPlugin.java
+++ src/main/java/com/cloudbees/jenkins/support/SupportPlugin.java
@@ -81,7 +81,6 @@ import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.FilterOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintStream;
