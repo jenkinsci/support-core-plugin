28bdf186dc30efcfaea4bbd652ebb82f96aaaabc
Allan Burdajewicz <allan.burdajewicz@gmail.com>
2021-11-01 12:57:37 +1000

    [chore] Remove powermock that breaks CI

--- src/test/java/com/cloudbees/jenkins/support/impl/OtherLogsTest.java
+++ src/test/java/com/cloudbees/jenkins/support/impl/OtherLogsTest.java
@@ -4,20 +4,14 @@
 package com.cloudbees.jenkins.support.impl;
 
 import com.cloudbees.jenkins.support.SupportTestUtils;
-import com.cloudbees.jenkins.support.api.Component;
-import hudson.ExtensionList;
 import jenkins.model.Jenkins;
 import org.assertj.core.api.Assertions;
 import org.hamcrest.Matchers;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.jvnet.hudson.test.JenkinsRule;
+import org.mockito.MockedStatic;
 import org.mockito.Mockito;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PowerMockIgnore;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
 
 import java.io.File;
 import java.io.IOException;
@@ -26,18 +20,15 @@ import java.nio.file.Files;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Objects;
+import java.util.function.Consumer;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static org.powermock.api.mockito.PowerMockito.mock;
-import static org.powermock.api.mockito.PowerMockito.when;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.when;
 
-@RunWith(PowerMockRunner.class)
-@PrepareForTest({OtherLogs.class, GCLogs.class, Jenkins.class})
-// Need to ignore some packages due to Powermockito issue with JDK 11 https://github.com/powermock/powermock/issues/864
-@PowerMockIgnore({ "javax.xml.*" })
 public class OtherLogsTest {
 
     @Rule
@@ -45,9 +36,10 @@ public class OtherLogsTest {
 
     @Test
     public void testOtherLogsContentEmpty() {
-        String otherLogs = SupportTestUtils.invokeComponentToString(
-                Objects.requireNonNull(ExtensionList.lookup(Component.class).get(OtherLogs.class)));
-        assertTrue("Should not write anything", otherLogs.isEmpty());
+        mockFinderAndGcLogs(finder -> {
+            String otherLogs = SupportTestUtils.invokeComponentToString(new OtherLogs());
+            assertTrue("Should not write anything", otherLogs.isEmpty());
+        });
     }
 
     @Test
@@ -55,9 +47,7 @@ public class OtherLogsTest {
         File testFile = new File(j.getInstance().getRootDir(), "test.log");
         Files.write(testFile.toPath(), Collections.singletonList("This is a test from root dir"), 
                 Charset.defaultCharset());
-        
-        String otherLogs = SupportTestUtils.invokeComponentToString(
-                Objects.requireNonNull(ExtensionList.lookup(Component.class).get(OtherLogs.class)));
+        String otherLogs = SupportTestUtils.invokeComponentToString(new OtherLogs());
         assertFalse("Should collect *.log under the root dir", otherLogs.isEmpty());
         assertThat(otherLogs , Matchers.containsString("This is a test from root dir"));
     }
@@ -71,19 +61,18 @@ public class OtherLogsTest {
         File tmpFile = File.createTempFile("gclogs", ".log", j.getInstance().getRootDir());
         Files.write(tmpFile.toPath(), Collections.singletonList("This is a GC file"));
 
-        GCLogs.VmArgumentFinder finder = mockFinderAndGcLogs();
+        mockFinderAndGcLogs(finder -> {
+            if (SupportTestUtils.isJava8OrBelow()) {
+                when(finder.findVmArgument(GCLogs.GCLOGS_JRE_SWITCH)).thenReturn(GCLogs.GCLOGS_JRE_SWITCH + tmpFile.getAbsolutePath());
+            } else {
+                when(finder.findVmArgument(GCLogs.GCLOGS_JRE9_SWITCH)).thenReturn(GCLogs.GCLOGS_JRE9_SWITCH + "*:file=" + tmpFile.getAbsolutePath());
+                assertContentContainsFiles(Collections.singletonList("other-logs/test.log"));
 
-        if (SupportTestUtils.isJava8OrBelow()) {
-            when(finder.findVmArgument(GCLogs.GCLOGS_JRE_SWITCH)).thenReturn(GCLogs.GCLOGS_JRE_SWITCH + tmpFile.getAbsolutePath());
+                // The file locations may be wrapped with quotes
+                when(finder.findVmArgument(GCLogs.GCLOGS_JRE9_SWITCH)).thenReturn(GCLogs.GCLOGS_JRE9_SWITCH + "*:file=\"" + tmpFile.getAbsolutePath() + "\"");
+            }
             assertContentContainsFiles(Collections.singletonList("other-logs/test.log"));
-        } else {
-            when(finder.findVmArgument(GCLogs.GCLOGS_JRE9_SWITCH)).thenReturn(GCLogs.GCLOGS_JRE9_SWITCH + "*:file=" + tmpFile.getAbsolutePath());
-            assertContentContainsFiles(Collections.singletonList("other-logs/test.log"));
-
-            // The file locations may be wrapped with quotes
-            when(finder.findVmArgument(GCLogs.GCLOGS_JRE9_SWITCH)).thenReturn(GCLogs.GCLOGS_JRE9_SWITCH + "*:file=\"" + tmpFile.getAbsolutePath()+ "\"");
-            assertContentContainsFiles(Collections.singletonList("other-logs/test.log"));
-        }
+        });
     }
 
     @Test
@@ -98,23 +87,23 @@ public class OtherLogsTest {
             Files.write(tmpFile.toPath(), Collections.singletonList("This is a GC file"));
         }
 
-        GCLogs.VmArgumentFinder finder = mockFinderAndGcLogs();
 
-        if(SupportTestUtils.isJava8OrBelow()) {
-            when(finder.findVmArgument(GCLogs.GCLOGS_ROTATION_SWITCH)).thenReturn(GCLogs.GCLOGS_ROTATION_SWITCH);
-            when(finder.findVmArgument(GCLogs.GCLOGS_JRE_SWITCH)).thenReturn(GCLogs.GCLOGS_JRE_SWITCH
-                + rootDir.getAbsolutePath() + File.separator + "gc.log");
+        mockFinderAndGcLogs(finder -> {
+            if (SupportTestUtils.isJava8OrBelow()) {
+                when(finder.findVmArgument(GCLogs.GCLOGS_ROTATION_SWITCH)).thenReturn(GCLogs.GCLOGS_ROTATION_SWITCH);
+                when(finder.findVmArgument(GCLogs.GCLOGS_JRE_SWITCH)).thenReturn(GCLogs.GCLOGS_JRE_SWITCH
+                    + rootDir.getAbsolutePath() + File.separator + "gc.log");
+            } else {
+                when(finder.findVmArgument(GCLogs.GCLOGS_JRE9_SWITCH)).thenReturn(GCLogs.GCLOGS_JRE9_SWITCH + "*:file="
+                    + rootDir.getAbsolutePath() + File.separator + "gc.log.%p" + ":filecount=10,filesize=50m");
+                assertContentContainsFiles(Collections.singletonList("other-logs/test.log"));
+    
+                // The file locations may be wrapped with quotes
+                when(finder.findVmArgument(GCLogs.GCLOGS_JRE9_SWITCH)).thenReturn(GCLogs.GCLOGS_JRE9_SWITCH + "*:file=\""
+                    + rootDir.getAbsolutePath() + File.separator + "gc.log.%p\":filecount=10,filesize=50m");
+            }
             assertContentContainsFiles(Collections.singletonList("other-logs/test.log"));
-        } else {
-            when(finder.findVmArgument(GCLogs.GCLOGS_JRE9_SWITCH)).thenReturn(GCLogs.GCLOGS_JRE9_SWITCH + "*:file="
-                + rootDir.getAbsolutePath() + File.separator + "gc.log.%p" + ":filecount=10,filesize=50m");
-            assertContentContainsFiles(Collections.singletonList("other-logs/test.log"));
-
-            // The file locations may be wrapped with quotes
-            when(finder.findVmArgument(GCLogs.GCLOGS_JRE9_SWITCH)).thenReturn(GCLogs.GCLOGS_JRE9_SWITCH + "*:file=\""
-                + rootDir.getAbsolutePath() + File.separator + "gc.log.%p\":filecount=10,filesize=50m");
-            assertContentContainsFiles(Collections.singletonList("other-logs/test.log"));
-        }
+        });
     }
 
     @Test
@@ -130,21 +119,20 @@ public class OtherLogsTest {
             Files.write(tmpFile.toPath(), Collections.singletonList("This is a GC file"));
         }
 
-        GCLogs.VmArgumentFinder finder = mockFinderAndGcLogs();
+        mockFinderAndGcLogs(finder -> {
+            if (SupportTestUtils.isJava8OrBelow()) {
+                when(finder.findVmArgument(GCLogs.GCLOGS_JRE_SWITCH)).thenReturn(GCLogs.GCLOGS_JRE_SWITCH
+                    + new File(rootDir, "gc.%t.%p.log").getAbsolutePath());
+            } else {
+                when(finder.findVmArgument(GCLogs.GCLOGS_JRE9_SWITCH)).thenReturn(GCLogs.GCLOGS_JRE9_SWITCH + "*:file="
+                    + rootDir.getAbsolutePath() + File.separator + "gc.%t.%p.log");
+                assertContentContainsFiles(Collections.singletonList("other-logs/test.log"));
 
-        if(SupportTestUtils.isJava8OrBelow()) {
-            when(finder.findVmArgument(GCLogs.GCLOGS_JRE_SWITCH)).thenReturn(GCLogs.GCLOGS_JRE_SWITCH
-                + new File(rootDir, "gc.%t.%p.log").getAbsolutePath());
+                when(finder.findVmArgument(GCLogs.GCLOGS_JRE9_SWITCH)).thenReturn(GCLogs.GCLOGS_JRE9_SWITCH + "*:file=\""
+                    + rootDir.getAbsolutePath() + File.separator + "gc.%t.%p.log\"");
+            }
             assertContentContainsFiles(Collections.singletonList("other-logs/test.log"));
-        } else {
-            when(finder.findVmArgument(GCLogs.GCLOGS_JRE9_SWITCH)).thenReturn(GCLogs.GCLOGS_JRE9_SWITCH + "*:file="
-                + rootDir.getAbsolutePath() + File.separator + "gc.%t.%p.log");
-            assertContentContainsFiles(Collections.singletonList("other-logs/test.log"));
-
-            when(finder.findVmArgument(GCLogs.GCLOGS_JRE9_SWITCH)).thenReturn(GCLogs.GCLOGS_JRE9_SWITCH + "*:file=\""
-                + rootDir.getAbsolutePath() + File.separator + "gc.%t.%p.log\"");
-            assertContentContainsFiles(Collections.singletonList("other-logs/test.log"));
-        }
+        });
     }
     
     @Test
@@ -159,25 +147,24 @@ public class OtherLogsTest {
             Files.write(new File(rootDir, "gc" + System.currentTimeMillis() + ".log." + count).toPath(), 
                 Collections.singletonList("This is a GC file"));
         }
-        
-        GCLogs.VmArgumentFinder finder = mockFinderAndGcLogs();
 
-        if(SupportTestUtils.isJava8OrBelow()) {
-            when(finder.findVmArgument(GCLogs.GCLOGS_JRE_SWITCH)).thenReturn(GCLogs.GCLOGS_JRE_SWITCH 
-                + new File(rootDir, "gc%p.log").getAbsolutePath());
-            when(finder.findVmArgument(GCLogs.GCLOGS_ROTATION_SWITCH)).thenReturn(GCLogs.GCLOGS_ROTATION_SWITCH);
-            assertContentContainsFiles(Collections.singletonList("other-logs/test.log"));
-        } else {
+        mockFinderAndGcLogs(finder -> {
+            if (SupportTestUtils.isJava8OrBelow()) {
+                when(finder.findVmArgument(GCLogs.GCLOGS_JRE_SWITCH)).thenReturn(GCLogs.GCLOGS_JRE_SWITCH
+                    + new File(rootDir, "gc%p.log").getAbsolutePath());
+                when(finder.findVmArgument(GCLogs.GCLOGS_ROTATION_SWITCH)).thenReturn(GCLogs.GCLOGS_ROTATION_SWITCH);
+            } else {
 
-            when(finder.findVmArgument(GCLogs.GCLOGS_JRE9_SWITCH)).thenReturn(GCLogs.GCLOGS_JRE9_SWITCH + "*:file="
-                + rootDir.getAbsolutePath() + File.separator + "gc%t.log.%p" + ":filecount=10,filesize=50m");
-            assertContentContainsFiles(Collections.singletonList("other-logs/test.log"));
+                when(finder.findVmArgument(GCLogs.GCLOGS_JRE9_SWITCH)).thenReturn(GCLogs.GCLOGS_JRE9_SWITCH + "*:file="
+                    + rootDir.getAbsolutePath() + File.separator + "gc%t.log.%p" + ":filecount=10,filesize=50m");
+                assertContentContainsFiles(Collections.singletonList("other-logs/test.log"));
 
-            // The file locations may be wrapped with quotes
-            when(finder.findVmArgument(GCLogs.GCLOGS_JRE9_SWITCH)).thenReturn(GCLogs.GCLOGS_JRE9_SWITCH + "*:file=\""
-                + rootDir.getAbsolutePath() + File.separator + "gc%t.log.%p\"" + ":filecount=10,filesize=50m");
+                // The file locations may be wrapped with quotes
+                when(finder.findVmArgument(GCLogs.GCLOGS_JRE9_SWITCH)).thenReturn(GCLogs.GCLOGS_JRE9_SWITCH + "*:file=\""
+                    + rootDir.getAbsolutePath() + File.separator + "gc%t.log.%p\"" + ":filecount=10,filesize=50m");
+            }
             assertContentContainsFiles(Collections.singletonList("other-logs/test.log"));
-        }
+        });
     }
 
     @Test
@@ -193,40 +180,40 @@ public class OtherLogsTest {
             Files.write(tmpFile.toPath(), Collections.singletonList("This is a GC file"));
         }
 
-        GCLogs.VmArgumentFinder finder = mockFinderAndGcLogs();
+        mockFinderAndGcLogs(finder -> {
+            // Configure GC Logs to go under $JENKINS_ROOT/gc/
+            if(SupportTestUtils.isJava8OrBelow()) {
+                when(finder.findVmArgument(GCLogs.GCLOGS_ROTATION_SWITCH)).thenReturn(GCLogs.GCLOGS_ROTATION_SWITCH);
+                when(finder.findVmArgument(GCLogs.GCLOGS_JRE_SWITCH)).thenReturn(GCLogs.GCLOGS_JRE_SWITCH
+                    + rootDir.getAbsolutePath() + File.separator + "gc" + File.separator + "gc.log");
+            } else {
+                when(finder.findVmArgument(GCLogs.GCLOGS_JRE9_SWITCH)).thenReturn(GCLogs.GCLOGS_JRE9_SWITCH + "*:file="
+                    + rootDir.getAbsolutePath() + File.separator + "gc" + File.separator + "gc.log.%p" + ":filecount=10,filesize=50m");
+                assertContentContainsFiles(Arrays.asList("other-logs/test.log", "other-logs/gc.log.0", "other-logs/gc.log.1",
+                    "other-logs/gc.log.2", "other-logs/gc.log.3", "other-logs/gc.log.4"));
 
-        // Configure GC Logs to go under $JENKINS_ROOT/gc/
-        if(SupportTestUtils.isJava8OrBelow()) {
-            when(finder.findVmArgument(GCLogs.GCLOGS_ROTATION_SWITCH)).thenReturn(GCLogs.GCLOGS_ROTATION_SWITCH);
-            when(finder.findVmArgument(GCLogs.GCLOGS_JRE_SWITCH)).thenReturn(GCLogs.GCLOGS_JRE_SWITCH
-                + rootDir.getAbsolutePath() + File.separator + "gc" + File.separator + "gc.log");
+                // The file locations may be wrapped with quotes
+                when(finder.findVmArgument(GCLogs.GCLOGS_JRE9_SWITCH)).thenReturn(GCLogs.GCLOGS_JRE9_SWITCH + "*:file=\""
+                    + rootDir.getAbsolutePath() + File.separator + "gc" + File.separator + "gc.log.%p\":filecount=10,filesize=50m");
+            }
             assertContentContainsFiles(Arrays.asList("other-logs/test.log", "other-logs/gc.log.0", "other-logs/gc.log.1",
                 "other-logs/gc.log.2", "other-logs/gc.log.3", "other-logs/gc.log.4"));
-        } else {
-            when(finder.findVmArgument(GCLogs.GCLOGS_JRE9_SWITCH)).thenReturn(GCLogs.GCLOGS_JRE9_SWITCH + "*:file="
-                + rootDir.getAbsolutePath() + File.separator + "gc" + File.separator + "gc.log.%p" + ":filecount=10,filesize=50m");
-            assertContentContainsFiles(Arrays.asList("other-logs/test.log", "other-logs/gc.log.0", "other-logs/gc.log.1",
-                "other-logs/gc.log.2", "other-logs/gc.log.3", "other-logs/gc.log.4"));
-
-            // The file locations may be wrapped with quotes
-            when(finder.findVmArgument(GCLogs.GCLOGS_JRE9_SWITCH)).thenReturn(GCLogs.GCLOGS_JRE9_SWITCH + "*:file=\""
-                + rootDir.getAbsolutePath() + File.separator + "gc" + File.separator + "gc.log.%p\":filecount=10,filesize=50m");
-            assertContentContainsFiles(Arrays.asList("other-logs/test.log", "other-logs/gc.log.0", "other-logs/gc.log.1",
-                "other-logs/gc.log.2", "other-logs/gc.log.3", "other-logs/gc.log.4"));
-        }
+        });
     }
 
     private void assertContentContainsFiles(Collection<String> fileNames) {
-        Assertions.assertThat(SupportTestUtils.invokeComponentToMap(
-            ExtensionList.lookupSingleton((Class<? extends Component>) OtherLogs.class)))
+        Assertions.assertThat(SupportTestUtils.invokeComponentToMap(new OtherLogs()))
                 .containsOnlyKeys(fileNames);
     }
     
-    private GCLogs.VmArgumentFinder mockFinderAndGcLogs() {
+    private GCLogs.VmArgumentFinder mockFinderAndGcLogs(Consumer<GCLogs.VmArgumentFinder> consumer) {
         GCLogs.VmArgumentFinder finder = mock(GCLogs.VmArgumentFinder.class);
         GCLogs gcLogs = new GCLogs(finder);
-        PowerMockito.mockStatic(Jenkins.class, Mockito.CALLS_REAL_METHODS);
-        when(Jenkins.lookup(GCLogs.class)).thenReturn(gcLogs);
+        try(MockedStatic<Jenkins> jenkinsMock = mockStatic(Jenkins.class, Mockito.CALLS_REAL_METHODS)) {
+            jenkinsMock.when(() -> Jenkins.lookup(GCLogs.class)).thenReturn(gcLogs);
+            jenkinsMock.when(() -> Jenkins.lookup(OtherLogs.class)).thenReturn(new OtherLogs());
+            consumer.accept(finder);
+        }
         return finder;
     }
 }
