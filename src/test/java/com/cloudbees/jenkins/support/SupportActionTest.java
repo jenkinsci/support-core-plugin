f982364224c8fc77cad768f27b0ee54fff53fa7c
Allan Burdajewicz <allan.burdajewicz@gmail.com>
2021-11-29 11:52:48 +1000

    [chore] Address deprecated usage ExpectedException.node

--- src/test/java/com/cloudbees/jenkins/support/SupportActionTest.java
+++ src/test/java/com/cloudbees/jenkins/support/SupportActionTest.java
@@ -25,7 +25,6 @@ import org.junit.Assume;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 import org.junit.rules.TemporaryFolder;
 import org.jvnet.hudson.test.Issue;
 import org.jvnet.hudson.test.JenkinsRule;
@@ -63,6 +62,7 @@ import static org.hamcrest.Matchers.startsWith;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
@@ -72,9 +72,6 @@ import static org.junit.Assert.fail;
 public class SupportActionTest {
     @Rule
     public JenkinsRule j = new JenkinsRule();
-
-    @Rule
-    public ExpectedException exceptionRule = ExpectedException.none();
 
     @Rule
     public TemporaryFolder temp = new TemporaryFolder();
@@ -285,9 +282,9 @@ public class SupportActionTest {
     }
 
     @Test
-    public void generateBundleFailsWhenNoParameter() throws IOException, SAXException {
-        exceptionRule.expectMessage(startsWith("Server returned HTTP response code: 400 for URL:"));
-        downloadBundle("/generateBundle");
+    public void generateBundleFailsWhenNoParameter() {
+        Exception exception = assertThrows(IOException.class, () -> downloadBundle("/generateBundle"));
+        assertThat(exception.getMessage(), startsWith("Server returned HTTP response code: 400 for URL:"));
     }
 
     @Test
