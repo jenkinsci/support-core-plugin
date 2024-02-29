ac39d187a9d140632127b5e76b80723ce981f787
Jesse Glick <jglick@cloudbees.com>
2024-02-29 16:38:21 -0500

    `SlaveLaunchLogsTest.onlineInboundAgent`

--- src/test/java/com/cloudbees/jenkins/support/impl/SlaveLaunchLogsTest.java
+++ src/test/java/com/cloudbees/jenkins/support/impl/SlaveLaunchLogsTest.java
@@ -10,6 +10,7 @@ import com.cloudbees.jenkins.support.SupportTestUtils;
 import hudson.ExtensionList;
 import org.junit.Rule;
 import org.junit.Test;
+import org.jvnet.hudson.test.InboundAgentRule;
 import org.jvnet.hudson.test.JenkinsRule;
 
 public class SlaveLaunchLogsTest {
@@ -17,17 +18,33 @@ public class SlaveLaunchLogsTest {
     @Rule
     public JenkinsRule j = new JenkinsRule();
 
+    @Rule
+    public InboundAgentRule inboundAgents = new InboundAgentRule();
+
     @Test
     public void onlineOutboundAgent() throws Exception {
         var s = j.createOnlineSlave();
         assertThat(
-                "reflects JenkinsRule.createComputerLauncher command and SlaveComputer.setChannel",
+                "reflects JenkinsRule.createComputerLauncher command & SlaveComputer.setChannel",
                 SupportTestUtils.invokeComponentToMap(ExtensionList.lookupSingleton(SlaveLaunchLogs.class))
                         .get("nodes/slave/" + s.getNodeName() + "/launchLogs/slave.log"),
                 allOf(
                         notNullValue(),
                         containsString("-XX:+PrintCommandLineFlags"),
                         containsString("Remoting version: ")));
+    }
+
+    @Test
+    public void onlineInboundAgent() throws Exception {
+        inboundAgents.createAgent(j, "remote");
+        assertThat(
+                "reflects DefaultJnlpSlaveReceiver.beforeChannel & SlaveComputer.setChannel",
+                SupportTestUtils.invokeComponentToMap(ExtensionList.lookupSingleton(SlaveLaunchLogs.class))
+                        .get("nodes/slave/remote/launchLogs/slave.log"),
+                allOf(
+                        notNullValue(),
+                        containsString("Inbound agent connected from"),
+                        containsString("Communication Protocol: JNLP4-connect")));
     }
 
     @Test
@@ -40,7 +57,6 @@ public class SlaveLaunchLogsTest {
     }
 
     // TODO logs from offline agent
-    // TODO logs from inbound agent
     // TODO logs from a deleted agent
     // TODO logs from multiple launch attempts of same agent
     // TODO honor SafeTimerTask.getLogsRoot (if applicable)
