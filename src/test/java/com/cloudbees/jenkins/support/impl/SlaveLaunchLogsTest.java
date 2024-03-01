3d0a97a11cca410f871f7f1256b92872f30c142d
Jesse Glick <jglick@cloudbees.com>
2024-03-01 15:06:13 -0500

    Exploring `SlaveLaunchLogs` behavior

--- src/test/java/com/cloudbees/jenkins/support/impl/SlaveLaunchLogsTest.java
+++ src/test/java/com/cloudbees/jenkins/support/impl/SlaveLaunchLogsTest.java
@@ -8,12 +8,25 @@ import static org.hamcrest.Matchers.notNullValue;
 
 import com.cloudbees.jenkins.support.SupportTestUtils;
 import hudson.ExtensionList;
+import hudson.model.Computer;
+import hudson.model.TaskListener;
+import hudson.slaves.ComputerListener;
+import hudson.slaves.SlaveComputer;
+import java.io.IOException;
+import jenkins.slaves.StandardOutputSwapper;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.InboundAgentRule;
 import org.jvnet.hudson.test.JenkinsRule;
+import org.jvnet.hudson.test.TestExtension;
 
 public class SlaveLaunchLogsTest {
+
+    static {
+        StandardOutputSwapper.disabled = true; // TODO JENKINS-65582 noisy
+        // TODO also suppress JnlpSlaveRestarterInstaller
+    }
 
     @Rule
     public JenkinsRule j = new JenkinsRule();
@@ -65,11 +78,51 @@ public class SlaveLaunchLogsTest {
                 "still includes something",
                 SupportTestUtils.invokeComponentToMap(ExtensionList.lookupSingleton(SlaveLaunchLogs.class))
                         .get("nodes/slave/" + name + "/launchLogs/slave.log"),
-                allOf(notNullValue(), containsString("Remoting version: ")));
+                allOf(notNullValue(), containsString("Remoting version: "), containsString("Connection terminated")));
     }
 
-    // TODO logs from a deleted agent
-    // TODO logs from multiple launch attempts of same agent
+    @Ignore("TODO not sure how to archive the logs before deletion from SlaveComputer.kill")
+    @Test
+    public void deletedAgent() throws Exception {
+        var s = j.createOnlineSlave();
+        var name = s.getNodeName();
+        s.toComputer().disconnect(null).get();
+        j.jenkins.removeNode(s);
+        assertThat(
+                "still includes something",
+                SupportTestUtils.invokeComponentToString(ExtensionList.lookupSingleton(SlaveLaunchLogs.class)),
+                allOf(notNullValue(), containsString("Remoting version: "), containsString("Connection terminated")));
+    }
+
+    @Test
+    public void multipleLaunchLogs() throws Exception {
+        var s = j.createOnlineSlave();
+        var name = s.getNodeName();
+        s.toComputer().disconnect(null).get();
+        Thread.sleep(1000); // TODO otherwise log is not flushed?
+        s.toComputer().connect(false).get();
+        assertThat(
+                "notes both launch logs",
+                SupportTestUtils.invokeComponentToString(ExtensionList.lookupSingleton(SlaveLaunchLogs.class)),
+                allOf(
+                        notNullValue(),
+                        containsString("Launch attempt #1"),
+                        containsString("Connection terminated"),
+                        containsString("Launch attempt #2")));
+    }
+
+    @TestExtension("multipleLaunchLogs")
+    public static final class DistinctiveLaunchMessage extends ComputerListener {
+        int count;
+
+        @Override
+        public void onOnline(Computer c, TaskListener listener) throws IOException, InterruptedException {
+            if (c instanceof SlaveComputer) {
+                listener.getLogger().println("Launch attempt #" + ++count);
+            }
+        }
+    }
+
     // TODO honor SafeTimerTask.getLogsRoot (if applicable)
     // TODO rotation of old or excessively long logs
 
