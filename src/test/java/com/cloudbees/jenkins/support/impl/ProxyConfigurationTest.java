540929a0d090af2e30f0c7e20ba272c658762922
Allan Burdajewicz <aburdajewicz@cloudbees.com>
2024-08-12 16:42:58 +1000

    [JENKINS-73599] Externlize Proxy details in its own component + fix markdowns

---
+++ src/test/java/com/cloudbees/jenkins/support/impl/ProxyConfigurationTest.java
@@ -0,0 +1,47 @@
+package com.cloudbees.jenkins.support.impl;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import com.cloudbees.jenkins.support.SupportTestUtils;
+import com.cloudbees.jenkins.support.api.Component;
+import hudson.ExtensionList;
+import hudson.ProxyConfiguration;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import org.junit.Rule;
+import org.junit.Test;
+import org.jvnet.hudson.test.Issue;
+import org.jvnet.hudson.test.JenkinsRule;
+
+public class ProxyConfigurationTest {
+
+    @Rule
+    public JenkinsRule j = new JenkinsRule();
+
+    @Test
+    @Issue({"JENKINS-68008", "JENKINS-73599"})
+    public void testProxyContent() {
+        List<String> noProxyHosts = Arrays.asList(".server.com", "*.example.com");
+        j.jenkins.setProxy(new ProxyConfiguration(
+                "proxy.server.com",
+                1234,
+                "proxyUser",
+                "proxyPass",
+                String.join("\n", noProxyHosts),
+                "http://localhost:8080"));
+
+        String ucMdToString =
+                SupportTestUtils.invokeComponentToString(Objects.requireNonNull(ExtensionList.lookup(Component.class)
+                        .get(com.cloudbees.jenkins.support.impl.ProxyConfiguration.class)));
+        assertThat(ucMdToString, containsString(" - Host: `proxy.server.com`"));
+        assertThat(ucMdToString, containsString(" - Port: `1234`"));
+        assertThat(ucMdToString, not(containsString("proxyUser")));
+        assertThat(ucMdToString, not(containsString("proxyPass")));
+        for (String noProxyHost : noProxyHosts) {
+            assertThat(ucMdToString, containsString(" * `" + noProxyHost + "`"));
+        }
+    }
+}
