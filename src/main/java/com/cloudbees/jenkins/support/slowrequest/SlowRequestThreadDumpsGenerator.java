128b8754fa29c8615a623827007cc4624fa6c099
Ignacio Roncero <61473809+ironcerocloudbees@users.noreply.github.com>
2024-08-19 08:56:17 +0200

    Update src/main/java/com/cloudbees/jenkins/support/slowrequest/SlowRequestThreadDumpsGenerator.java
    Co-authored-by: Allan Burdajewicz <allan.burdajewicz@gmail.com>

--- src/main/java/com/cloudbees/jenkins/support/slowrequest/SlowRequestThreadDumpsGenerator.java
+++ src/main/java/com/cloudbees/jenkins/support/slowrequest/SlowRequestThreadDumpsGenerator.java
@@ -75,7 +75,7 @@ public class SlowRequestThreadDumpsGenerator extends Thread {
     /**
      * Provide a means to disable the slow request thread dump checker.
      */
-    public static boolean DISABLED = Boolean.getBoolean(SlowRequestThreadDumpsGenerator.class.getName() + ".DISABLED");
+    public static final boolean DISABLED = Boolean.getBoolean(SlowRequestThreadDumpsGenerator.class.getName() + ".DISABLED");
 
     private final SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd-HHmmss.SSS");
 
