fee301d5579deed283ce59a341af7ef9dc36c9db
Ignacio Roncero <ironcero@cloudbees.com>
2024-08-20 08:17:17 +0200

    JENKINS-73431: Spotless

--- src/main/java/com/cloudbees/jenkins/support/slowrequest/SlowRequestThreadDumpsGenerator.java
+++ src/main/java/com/cloudbees/jenkins/support/slowrequest/SlowRequestThreadDumpsGenerator.java
@@ -76,8 +76,7 @@ public class SlowRequestThreadDumpsGenerator extends Thread {
     /**
      * Provide a means to disable the slow request thread dump checker.
      */
-    public static boolean DISABLED =
-            Boolean.getBoolean(SlowRequestThreadDumpsGenerator.class.getName() + ".DISABLED");
+    public static boolean DISABLED = Boolean.getBoolean(SlowRequestThreadDumpsGenerator.class.getName() + ".DISABLED");
 
     private final SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd-HHmmss.SSS");
 
