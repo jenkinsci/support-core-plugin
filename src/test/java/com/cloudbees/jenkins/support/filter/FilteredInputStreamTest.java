77b4ff01e23f67b8875fe44ff49f94434dd9626d
Basil Crow <me@basilcrow.com>
2024-10-18 09:13:17 -0700

    More efficient filtering of input streams

---
+++ src/test/java/com/cloudbees/jenkins/support/filter/FilteredInputStreamTest.java
@@ -0,0 +1,51 @@
+package com.cloudbees.jenkins.support.filter;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import org.junit.Test;
+
+public class FilteredInputStreamTest {
+
+    @Test
+    public void testReadSingleLine() throws IOException {
+        String input = "foo bar";
+        String output;
+        try (FilteredInputStream fis = new FilteredInputStream(
+                new ByteArrayInputStream(input.getBytes(StandardCharsets.UTF_8)),
+                StandardCharsets.UTF_8,
+                String::toUpperCase)) {
+            output = new String(fis.readAllBytes(), StandardCharsets.UTF_8);
+        }
+        assertEquals("FOO BAR" + System.lineSeparator(), output);
+    }
+
+    @Test
+    public void testReadMultipleLines() throws IOException {
+        String input = "Line 1\nLine 2\nLine 3";
+        String output;
+        try (FilteredInputStream fis = new FilteredInputStream(
+                new ByteArrayInputStream(input.getBytes(StandardCharsets.UTF_8)),
+                StandardCharsets.UTF_8,
+                String::toUpperCase)) {
+            output = new String(fis.readAllBytes(), StandardCharsets.UTF_8);
+        }
+        assertEquals(
+                "LINE 1" + System.lineSeparator() + "LINE 2" + System.lineSeparator() + "LINE 3"
+                        + System.lineSeparator(),
+                output);
+    }
+
+    @Test
+    public void testReadEmptyStream() throws IOException {
+        byte[] input = new byte[0];
+        String output;
+        try (FilteredInputStream fis =
+                new FilteredInputStream(new ByteArrayInputStream(input), StandardCharsets.UTF_8, String::toUpperCase)) {
+            output = new String(fis.readAllBytes(), StandardCharsets.UTF_8);
+        }
+        assertEquals("", output);
+    }
+}
