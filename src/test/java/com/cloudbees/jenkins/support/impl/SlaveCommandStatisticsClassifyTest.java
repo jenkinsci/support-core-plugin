58a4bc951573afe4bf7a5bd206424ebe487c3c4b
Albert Puig <apuig@cloudbees.com>
2025-12-31 15:17:29 +0100

    fix spotless

--- src/test/java/com/cloudbees/jenkins/support/impl/SlaveCommandStatisticsClassifyTest.java
+++ src/test/java/com/cloudbees/jenkins/support/impl/SlaveCommandStatisticsClassifyTest.java
@@ -95,7 +95,6 @@ public class SlaveCommandStatisticsClassifyTest {
                 expected,
                 classify("UserRequest:RemoteLaunchCallable[cmd=[docker, --env, TIMESTAMP=%d]]@deadbeef"
                         .formatted(System.currentTimeMillis())));
-
     }
 
     @Test
